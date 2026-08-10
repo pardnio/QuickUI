a0b1998c249f2232c8a264ae72e73a5dac77209d
pardnchiu <dev@pardn.io>
2026-08-10 14:04:10 +0800

    refactor: Restore source files from obfuscation

--- src/interface.ts
+++ src/interface.ts
@@ -1,11 +1,11 @@
 interface Window {
-    QUI: any;
-};
+  QUI: any;
+}
 
 type Patch =
-    | { type: "CREATE"; vdom: vDOM | string; index: number[] }
-    | { type: "APPEND"; vdom: vDOM | string; index: number[] }
-    | { type: "REPLACE"; vdom: vDOM; index: number[] }
-    | { type: "TEXT"; vdom: vDOM; value: string; index: number[] }
-    | { type: "PROP"; vdom: vDOM; key: string; value: string | null | undefined; index: number[] }
-    | { type: "REMOVE"; index: number[] };
+  | { type: "CREATE"; vdom: vDOM | string; index: number[] }
+  | { type: "APPEND"; vdom: vDOM | string; index: number[] }
+  | { type: "REPLACE"; vdom: vDOM; index: number[] }
+  | { type: "TEXT"; vdom: vDOM; value: string; index: number[] }
+  | { type: "PROP"; vdom: vDOM; key: string; value: string | null | undefined; index: number[] }
+  | { type: "REMOVE"; index: number[] };
